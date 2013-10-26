dafee48686ac49389f54cdbb83ff549bc024b4e4
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-10-26 15:44:03 +0200

    Extend HTML with template inheritance.

--- reddit_undo/settings.py
+++ reddit_undo/settings.py
@@ -121,7 +121,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    './undo/templates/',
+    os.path.join(PROJECT_DIR, '../undo/templates/undo/'),
 )
 
 INSTALLED_APPS = (
